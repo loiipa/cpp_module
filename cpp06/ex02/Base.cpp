052c969daa14f98a092aa371282f0a1019f6264c
cjang <cjang@student.42seoul.kr>
2022-02-25 06:37:44 +0900

    cpp 06 ex02 fin

--- cpp06/ex02/Base.cpp
+++ cpp06/ex02/Base.cpp
@@ -6,7 +6,7 @@
 /*   By: cjang <cjang@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/24 17:13:40 by cjang             #+#    #+#             */
-/*   Updated: 2022/02/24 17:54:29 by cjang            ###   ########.fr       */
+/*   Updated: 2022/02/25 06:14:07 by cjang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,13 +27,13 @@ Base *	Base::generate( void )
 	switch (idx)
 	{
 		case 0:
-			std::cout << "A" << std::endl;
+			std::cout << "I'm class A" << std::endl;
 			return new A();
 		case 1:
-			std::cout << "B" << std::endl;
+			std::cout << "I'm class B" << std::endl;
 			return new B();
 		case 2:
-			std::cout << "C" << std::endl;
+			std::cout << "I'm class C" << std::endl;
 			return new C();
 	}
 	return NULL;
@@ -42,24 +42,56 @@ Base *	Base::generate( void )
 void	Base::identify( Base* p )
 {
 	A*	a;
-	// B*	b;
-	// C*	c;
+	B*	b;
+	C*	c;
 
-	try
-	{
-		a = dynamic_cast<A*>(p);
-		std::cout << "a" << std::endl;
-	}
-	catch (const std::exception& e)
-	{
-		std::cerr << e.what() << '\n';
-	}
-	
+	a = dynamic_cast<A*>(p);
+	if (a != NULL)
+		std::cout << "A" << std::endl;
+
+	b = dynamic_cast<B*>(p);
+	if (b != NULL)
+		std::cout << "B" << std::endl;
+
+	c = dynamic_cast<C*>(p);
+	if (c != NULL)
+		std::cout << "C" << std::endl;
+
 	return ;
 }
 
 void	Base::identify( Base& p )
 {
-	(void) p;
+	try
+	{
+		A&	a = dynamic_cast<A&>(p);
+		(void) a;
+		std::cout << "A" << std::endl;
+	}
+	catch (std::bad_cast &bc)
+	{
+		;
+	}
+	
+	try
+	{
+		B&	b = dynamic_cast<B&>(p);
+		(void) b;
+		std::cout << "B" << std::endl;
+	}
+	catch (std::bad_cast &bc)
+	{
+		;
+	}
+	try
+	{
+		C&	c = dynamic_cast<C&>(p);
+		(void) c;
+		std::cout << "C" << std::endl;
+	}
+	catch (std::bad_cast &bc)
+	{
+		;
+	}
 	return ;
 }
--- cpp06/ex02/main.cpp
+++ cpp06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cjang <cjang@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/24 17:13:11 by cjang             #+#    #+#             */
-/*   Updated: 2022/02/24 17:50:59 by cjang            ###   ########.fr       */
+/*   Updated: 2022/02/25 06:25:07 by cjang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,20 @@
 
 int	main(void)
 {
-	Base	base1;
-	Base*	base2;
-	// Base*	base3;
-	// Base*	base4;
+	Base	base;
+	Base*	abc_ptr;
 
-	base2 = base1.generate();
-	base1.identify(base2);
+	abc_ptr = base.generate();
+	base.identify(abc_ptr);
+	base.identify(*abc_ptr);
+
+	std::cout << std::endl;
+	
+	A		a;
+
+	base.identify(&a);
+	base.identify(a);
+
+	delete abc_ptr;
+	return 0;
 }
